db11d556209b16b3256c1fdfe15412534c9af85c
dtrive02 <dtrive02@uoguelph.ca>
2026-02-28 15:30:33 -0500

    Update BookingStatus.java

--- src/ca/g26final/model/bookings/BookingStatus.java
+++ src/ca/g26final/model/bookings/BookingStatus.java
@@ -1,5 +1,8 @@
 package ca.g26final.model.bookings;
-
+// This enum stores the possible states of a booking.
+// CONFIRMED = user got a spot in the event
+// WAITLISTED = event is full, so user is waiting for a spot
+// CANCELLED = the booking was removed/cancelled
 public enum BookingStatus {
 CONFIRMED,
     WAITLISTED,
